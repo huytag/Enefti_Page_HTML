08cc9471aed132c6789d2556519ccb59d1020fb9
huytag <huytagicloud@gmail.com>
2022-05-10 17:39:23 +0700

    add code trending collection

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,8 +2,65 @@
   $(function () {
     $(document).ready(function () {
       liveSlickListTopFooter();
+      liveSlickListCollections();
     });
     
+
+    function liveSlickListCollections() {
+      $(".slick-collections")
+        .not(".slick-initialized")
+        .slick({
+          dots: false,
+          infinite: true,
+          speed: 300,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                dots: false,
+              },
+            },
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                dots: false,
+              },
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                dots: false,
+              },
+            },
+            {
+              breakpoint: 576,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+              },
+            },
+            {
+              breakpoint: 479,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+              },
+            },
+          ],
+        });
+    }
+
+
     function liveSlickListTopFooter() {
       $(".slick-list-top-header")
         .not(".slick-initialized")
